938eedf56278b3ffddb53414bc8d466363a0e47c
LeoDog896 <LeoDog896@hotmail.com>
2021-09-03 23:34:28 -0400

    Begin Build system refactor

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    kotlin("jvm") version "1.5.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("org.jetbrains.dokka") version "1.5.0"
+    kotlin("plugin.serialization") version "1.4.21"
+    `maven-publish`
+
+    // Apply the application plugin to add support for building a jar
+    java
+}
+
+repositories {
+    // Use mavenCentral
+    mavenCentral()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    compileOnly(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    compileOnly(kotlin("stdlib"))
+
+    // Use the Kotlin reflect library.
+    compileOnly(kotlin("reflect"))
+
+    // Use the JUpiter test library.
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
+}
+
+tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "16" }
+val compileKotlin: KotlinCompile by tasks
+
+configurations {
+    testImplementation {
+        extendsFrom(configurations.compileOnly.get())
+    }
+}
